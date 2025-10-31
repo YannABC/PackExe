a09c37d2d5ea782cc960632657eaf5c5000030aa
yanjun.pan <yanjun.pan@garena.com>
2025-10-31 22:00:45 +0800

    fix

--- pack_exe.bat
+++ pack_exe.bat
@@ -3,12 +3,14 @@ REM https://www.hipdf.cn/png-to-ico
 REM https://blog.csdn.net/qq_38893210/article/details/120721548
 REM https://blog.csdn.net/weixin_42837024/article/details/119782586
 
+cd /d "%~dp0"
 chcp 65001
 
 set software_name=Escape
 set dir_name=Exe
 set exe_name=Client.exe
-
+set icon_name=Icon.ico
+set temp_icon=C:\Temp\%icon_name%
 set config_file=pack_exe_config
 
 echo Setup=%dir_name%/%exe_name%>%config_file%
@@ -17,7 +19,8 @@ echo Silent=^1>>%config_file%
 echo Overwrite=^1>>%config_file%
 echo Update=U>>%config_file%
 
-WinRAR.exe a -s -z"%config_file%" -iiconc:./Icon.ico "./%software_name%.exe" ./%dir_name%
+copy /Y "%~dp0%icon_name%" "%temp_icon%" >nul
+WinRAR.exe a -s -z"%config_file%" "./%software_name%.exe" ./%dir_name% -iicon "%temp_icon%"
 
 del %config_file%
 
